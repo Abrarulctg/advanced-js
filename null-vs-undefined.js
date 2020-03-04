b3d0a69220e259d49b7a7b5167fdc34d0c4a1e5e
Abrarul Hoque <jubairctg99@gmail.com>
2020-03-04 18:46:21 +0600

    Null Vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+let pakhi;
+console.log (pakhi);
+
+
+function add (num1, num2){
+    console.log (num1+num2);
+    return
+}
+
+
+function add (num1, num2){
+    console.log (num1, num2);
+}
+
+
+/** const Name ={name:"Abrar", Cell:01846112525};
+console.log (Name); */ //this is Defined
+
+const Name ={name:"Abrar", Cell:01846112525};
+console.log (Name.AA);  //this is Undefined.(kono ekta kicu ami print koraite cacci, but, ta ekhane nai. tai eti undefined.)
+
+
+const name = undefined;
+console.log(name); //undefined define kore undefined kora jai..
+
+let ages=[11,22,33];
+console.log(ages[11]);
